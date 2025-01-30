83153a1e399c62f4f0bf9140c87e60c785ea4aee
Berkay <berkay132504@outlook.com>
2025-01-30 23:45:18 +0300

    c

--- Model/Calculator.java
+++ Model/Calculator.java
@@ -1,5 +1,98 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
+
 public class Calculator {
-    
+    private double result;
+    private double firstOperand;
+    private double secondOperand;
+    private String tempExpression;
+    private int index;
+    private Stack<Character> operatorStack;
+    private Stack<Double> operandStack;
+    private ArrayList<String> postfixExpression;
+
+    public Calculator(String expression) {
+        tempExpression = "";
+        index = 0;
+        operatorStack = new Stack<Character>();
+        postfixExpression = new ArrayList<String>();
+
+        while(index < expression.length()) {
+            if(Character.isDigit(expression.charAt(index)) || expression.charAt(index) == '.') {
+                while(index != expression.length() && (Character.isDigit(expression.charAt(index)) || expression.charAt(index) == '.')) {
+                    tempExpression += expression.charAt(index);
+                    index++;
+                }
+
+                postfixExpression.add(tempExpression);
+                tempExpression = "";
+            }
+            else if(expression.charAt(index) == '(') {
+                operatorStack.push(expression.charAt(index));
+                index++;
+            }
+            else if(expression.charAt(index) == ')') {
+                while(operatorStack.peek() != '(') {
+                    postfixExpression.add(String.valueOf(operatorStack.peek()));
+                    operatorStack.pop();
+                }
+                operatorStack.pop();
+                index++;
+            }
+            else {
+                while(!operatorStack.isEmpty() && operatorStack.peek() != '(' && precedenceIsGreater(operatorStack.peek(), expression.charAt(index))) {
+                    postfixExpression.add(String.valueOf(operatorStack.peek()));
+                    operatorStack.pop(); 
+                }
+
+                operatorStack.push(expression.charAt(index));
+                index++;
+            }
+        }    
+
+        while(!operatorStack.isEmpty()) {
+            postfixExpression.add(String.valueOf(operatorStack.peek()));
+            operatorStack.pop();
+        }
+
+        operandStack = new Stack<Double>();
+
+        for(int n = 0; n < postfixExpression.size(); n++) {
+            if(postfixExpression.get(n).equals("+") || postfixExpression.get(n).equals("-") || postfixExpression.get(n).equals("×") || postfixExpression.get(n).equals("÷")) {
+                secondOperand = operandStack.peek();
+                operandStack.pop();
+
+                firstOperand = operandStack.peek();
+                operandStack.pop();
+
+                if(postfixExpression.get(n).equals("+"))
+                    result = firstOperand + secondOperand;
+                else if(postfixExpression.get(n).equals("-"))
+                    result = firstOperand - secondOperand;
+                else if(postfixExpression.get(n).equals("×"))
+                    result = firstOperand * secondOperand;
+                else
+                    result = firstOperand / secondOperand;
+                    
+                operandStack.push(result);
+            }
+            else {
+                operandStack.push(Double.valueOf(postfixExpression.get(n)));
+            }
+        }
+    }
+
+    private static boolean precedenceIsGreater(char firstOperator, char secondOperator) {
+        if((firstOperator == '+' || firstOperator == '-') && (secondOperator == '×' || secondOperator == '÷'))
+            return false;
+        
+        return true;
+    }
+
+    public double getResult() {
+        return result;
+    }
 }
--- View/CalculatorApplication.java
+++ View/CalculatorApplication.java
@@ -1,11 +1,15 @@
 package View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import Model.Calculator;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
@@ -21,56 +25,135 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class CalculatorApplication extends Application {
-    VBox root;
-    HBox subroot1;
-    HBox subroot2;
-    HBox subroot3;
-    HBox subroot4;
-    HBox subroot5;
-    Button buttonForZero;
-    Button buttonForOne;
-    Button buttonForTwo;
-    Button buttonForThree;
-    Button buttonForFour;
-    Button buttonForFive;
-    Button buttonForSix;
-    Button buttonForSeven;
-    Button buttonForEight;
-    Button buttonForNine;
-    Button buttonForLeftParanthesis;
-    Button buttonForRightParanthesis;
-    Button buttonForSquareRoot;
-    Button buttonForAddition;
-    Button buttonForSubtraction;
-    Button buttonForDivision;
-    Button buttonForMultiplication;
-    Button buttonForAllClear;
-    Button buttonForPoint;
-    Button buttonForEquals;
-    TextField fieldForDisplaying;
+    private VBox root;
+    private HBox subroot1;
+    private HBox subroot2;
+    private HBox subroot3;
+    private HBox subroot4;
+    private HBox subroot5;
+    private Button buttonForZero;
+    private Button buttonForOne;
+    private Button buttonForTwo;
+    private Button buttonForThree;
+    private Button buttonForFour;
+    private Button buttonForFive;
+    private Button buttonForSix;
+    private Button buttonForSeven;
+    private Button buttonForEight;
+    private Button buttonForNine;
+    private Button buttonForLeftParanthesis;
+    private Button buttonForRightParanthesis;
+    private Button buttonForClearEntry;
+    private Button buttonForAddition;
+    private Button buttonForSubtraction;
+    private Button buttonForDivision;
+    private Button buttonForMultiplication;
+    private Button buttonForAllClear;
+    private Button buttonForPoint;
+    private Button buttonForEquals;
+    private TextField fieldForDisplaying;
+    private String displayInfo;
 
     @Override
     public void start(Stage stage) {
-        buttonForZero = createButton("0");
-        buttonForOne = createButton("1");
-        buttonForTwo = createButton("2");
-        buttonForThree = createButton("3");
-        buttonForFour = createButton("4");
-        buttonForFive = createButton("5");
-        buttonForSix = createButton("6");
-        buttonForSeven = createButton("7");
-        buttonForEight = createButton("8");
-        buttonForNine = createButton("9");
-        buttonForLeftParanthesis = createButton("(");
-        buttonForRightParanthesis = createButton(")");
-        buttonForSquareRoot = createButton("√");
-        buttonForAddition = createButton("+");
-        buttonForSubtraction = createButton("-");
-        buttonForDivision = createButton("÷");
-        buttonForMultiplication = createButton("×");
-        buttonForAllClear = createButton("AC");
-        buttonForPoint = createButton(".");
-        buttonForEquals = createButton("=");
+        displayInfo = "";
+
+        buttonForZero = createButtonForNumbers("0");
+        buttonForOne = createButtonForNumbers("1");
+        buttonForTwo = createButtonForNumbers("2");
+        buttonForThree = createButtonForNumbers("3");
+        buttonForFour = createButtonForNumbers("4");
+        buttonForFive = createButtonForNumbers("5");
+        buttonForSix = createButtonForNumbers("6");
+        buttonForSeven = createButtonForNumbers("7");
+        buttonForEight = createButtonForNumbers("8");
+        buttonForNine = createButtonForNumbers("9");
+
+        buttonForAddition = createButtonForOperationSigns("+");
+        buttonForSubtraction = createButtonForOperationSigns("-");
+        buttonForDivision = createButtonForOperationSigns("÷");
+        buttonForMultiplication = createButtonForOperationSigns("×");
+
+        buttonForLeftParanthesis = createButtonForOthers("(");
+        buttonForRightParanthesis = createButtonForOthers(")");
+        buttonForClearEntry = createButtonForOthers("CE");
+        buttonForAllClear = createButtonForOthers("AC");
+        buttonForPoint = createButtonForOthers(".");
+        buttonForEquals = createButtonForOthers("=");
+
+        buttonForLeftParanthesis.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if(displayInfo.length() == 0) {
+                    displayInfo += "(";
+                    fieldForDisplaying.setText(displayInfo);
+                }
+                
+                if(displayInfo.charAt(displayInfo.length() - 1) == '+' || displayInfo.charAt(displayInfo.length() - 1) == '-' || displayInfo.charAt(displayInfo.length() - 1) == '÷' || displayInfo.charAt(displayInfo.length() - 1) == '×' || displayInfo.charAt(displayInfo.length() - 1) == '√') {
+                    displayInfo += "(";
+                    fieldForDisplaying.setText(displayInfo);
+                }
+            }
+            
+        });
+
+        buttonForRightParanthesis.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if(LeftParanthesisCounter() != RightParanthesisCounter() && displayInfo.length() != 0 && (displayInfo.charAt(displayInfo.length() - 1) == '0' || displayInfo.charAt(displayInfo.length() - 1) == '1' || displayInfo.charAt(displayInfo.length() - 1) == '2' || displayInfo.charAt(displayInfo.length() - 1) == '3' || displayInfo.charAt(displayInfo.length() - 1) == '4' || displayInfo.charAt(displayInfo.length() - 1) == '5' || displayInfo.charAt(displayInfo.length() - 1) == '6' || displayInfo.charAt(displayInfo.length() - 1) == '7' || displayInfo.charAt(displayInfo.length() - 1) == '8' || displayInfo.charAt(displayInfo.length() - 1) == '9')) {
+                    displayInfo += ")";
+                    fieldForDisplaying.setText(displayInfo);
+                }
+            }
+            
+        });
+
+        buttonForClearEntry.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if(displayInfo.length() != 0) {
+                    displayInfo = displayInfo.substring(0, displayInfo.length() - 1);
+                    fieldForDisplaying.setText(displayInfo);  
+                }
+            }
+            
+        });
+
+        buttonForAllClear.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                displayInfo = "";
+                fieldForDisplaying.setText(displayInfo);
+            }
+            
+        });
+
+        buttonForPoint.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if(displayInfo.length() != 0 && (displayInfo.charAt(displayInfo.length() - 1) == '0' || displayInfo.charAt(displayInfo.length() - 1) == '1' || displayInfo.charAt(displayInfo.length() - 1) == '2' || displayInfo.charAt(displayInfo.length() - 1) == '3' || displayInfo.charAt(displayInfo.length() - 1) == '4' || displayInfo.charAt(displayInfo.length() - 1) == '5' || displayInfo.charAt(displayInfo.length() - 1) == '6' || displayInfo.charAt(displayInfo.length() - 1) == '7' || displayInfo.charAt(displayInfo.length() - 1) == '8' || displayInfo.charAt(displayInfo.length() - 1) == '9')) {
+                    displayInfo += ".";
+                    fieldForDisplaying.setText(displayInfo);
+                }
+            }
+            
+        });
+
+        buttonForEquals.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                Calculator calculator = new Calculator(displayInfo);
+                displayInfo = calculator.getResult() + "";
+                fieldForDisplaying.setText(displayInfo);
+            }
+            
+        });
 
         fieldForDisplaying = new TextField();
         fieldForDisplaying.setFont(Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 25));
@@ -79,7 +162,7 @@ public class CalculatorApplication extends Application {
         fieldForDisplaying.setStyle("-fx-text-fill: white");
 
         subroot1 = new HBox(10);
-        subroot1.getChildren().addAll(buttonForLeftParanthesis, buttonForRightParanthesis, buttonForSquareRoot, buttonForAllClear);
+        subroot1.getChildren().addAll(buttonForLeftParanthesis, buttonForRightParanthesis, buttonForClearEntry, buttonForAllClear);
 
         subroot2 = new HBox(10);
         subroot2.getChildren().addAll(buttonForSeven, buttonForEight, buttonForNine, buttonForDivision);
@@ -106,27 +189,124 @@ public class CalculatorApplication extends Application {
         stage.show();
     }
 
-    public static Button createButton(String text) {
+    private int LeftParanthesisCounter() {
+        int counter = 0;
+
+        for(int n = 0; n < displayInfo.length(); n++) {
+            if(displayInfo.charAt(n) == '(') {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    private int RightParanthesisCounter() {
+        int counter = 0;
+
+        for(int n = 0; n < displayInfo.length(); n++) {
+            if(displayInfo.charAt(n) == ')') {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    private Button createButtonForOthers(String text) {
         Button button =  new Button(text);
         button.setFont(Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 25));
-        button.setBackground(new Background(new BackgroundFill(Color.GREY, new CornerRadii(30), Insets.EMPTY)));
+        button.setBackground(new Background(new BackgroundFill(Color.DIMGREY, new CornerRadii(30), Insets.EMPTY)));
         button.setStyle("-fx-text-fill: white");
         button.setMinWidth(100);
 
-        button.setOnMouseEntered(new EventHandler<MouseEvent>() {
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
             @Override
             public void handle(MouseEvent event) {
-                button.setBackground(new Background(new BackgroundFill(Color.DIMGRAY, new CornerRadii(30), Insets.EMPTY)));
+                button.setBackground(new Background(new BackgroundFill(Color.GREY, new CornerRadii(30), Insets.EMPTY)));
+                
+                Timer myTimer = new Timer();
+                myTimer.schedule(new TimerTask(){
+
+                    @Override
+                    public void run() {
+                        button.setBackground(new Background(new BackgroundFill(Color.DIMGREY, new CornerRadii(30), Insets.EMPTY)));
+                    }
+
+                }, 100);
             }
             
         });
 
-        button.setOnMouseExited(new EventHandler<MouseEvent>() {
+        return button;
+    }
+
+    private Button createButtonForNumbers(String text) {
+        Button button =  new Button(text);
+        button.setFont(Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 25));
+        button.setBackground(new Background(new BackgroundFill(Color.DIMGREY, new CornerRadii(30), Insets.EMPTY)));
+        button.setStyle("-fx-text-fill: white");
+        button.setMinWidth(100);
+
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
             @Override
             public void handle(MouseEvent event) {
-                button.setBackground(new Background(new BackgroundFill(Color.GREY, new CornerRadii(30), Insets.EMPTY)));   
+                button.setBackground(new Background(new BackgroundFill(Color.GREY, new CornerRadii(30), Insets.EMPTY)));
+
+                if(displayInfo.length() == 0) {
+                    displayInfo += text;
+                    fieldForDisplaying.setText(displayInfo);
+                }
+                else if(displayInfo.charAt(displayInfo.length() - 1) != ')') {
+                    displayInfo += text;
+                    fieldForDisplaying.setText(displayInfo);
+                }
+                
+                Timer myTimer = new Timer();
+                myTimer.schedule(new TimerTask(){
+
+                    @Override
+                    public void run() {
+                        button.setBackground(new Background(new BackgroundFill(Color.DIMGREY, new CornerRadii(30), Insets.EMPTY)));
+                    }
+
+                }, 100);
+            }
+            
+        });
+
+        return button;
+    }
+
+    private Button createButtonForOperationSigns(String text) {
+        Button button =  new Button(text);
+        button.setFont(Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 25));
+        button.setBackground(new Background(new BackgroundFill(Color.DIMGREY, new CornerRadii(30), Insets.EMPTY)));
+        button.setStyle("-fx-text-fill: white");
+        button.setMinWidth(100);
+
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                button.setBackground(new Background(new BackgroundFill(Color.GREY, new CornerRadii(30), Insets.EMPTY)));
+
+                if(displayInfo.length() != 0 && (displayInfo.charAt(displayInfo.length() - 1) == ')' || displayInfo.charAt(displayInfo.length() - 1) == '0' || displayInfo.charAt(displayInfo.length() - 1) == '1' || displayInfo.charAt(displayInfo.length() - 1) == '2' || displayInfo.charAt(displayInfo.length() - 1) == '3' || displayInfo.charAt(displayInfo.length() - 1) == '4' || displayInfo.charAt(displayInfo.length() - 1) == '5' || displayInfo.charAt(displayInfo.length() - 1) == '6' || displayInfo.charAt(displayInfo.length() - 1) == '7' || displayInfo.charAt(displayInfo.length() - 1) == '8' || displayInfo.charAt(displayInfo.length() - 1) == '9')) {
+                    displayInfo += text;
+                    fieldForDisplaying.setText(displayInfo);
+                }
+                
+                Timer myTimer = new Timer();
+                myTimer.schedule(new TimerTask(){
+
+                    @Override
+                    public void run() {
+                        button.setBackground(new Background(new BackgroundFill(Color.DIMGREY, new CornerRadii(30), Insets.EMPTY)));
+                    }
+
+                }, 100);
             }
             
         });
